189032c733a5f60b0b814b5f85293e34ffa36c78
jxkimathi <judescriva8@gmail.com>
2025-02-11 18:39:52 +0300

    Modify the test_case for the read_book function

--- tests/test_books.py
+++ tests/test_books.py
@@ -51,6 +51,6 @@ def test_delete_book():
     response = client.get("/books/3")
     assert response.status_code == 404
 
-def test_read_book(client):
-    response = client.get(f"/api/v1/books/1")
-    assert response.status_code[200, 404]
+def test_read_book():
+    response = client.get("/api/v1/books/1")
+    assert response.status_code in [200, 404]
